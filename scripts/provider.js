edeae2394bf28a40f5512bff38d863c92e65d4ef
Jaraxus <Superseth794@gmail.com>
2023-02-12 12:55:22 +0100

    Remove unnecessary parts from provider script

--- scripts/provider.js
+++ scripts/provider.js
@@ -10,36 +10,8 @@ if (typeof window.ethereum !== 'undefined') {
 const provider = new ethers.providers.Web3Provider(window.ethereum)
 const signer = provider.getSigner();
 
-// Initialise the document objects
-const sendETH = document.querySelector('#sendETH');
-const addressTo = document.querySelector('#addressTo');
-const ETHAmount = document.querySelector('#ETHAmount');
-
-sendETH.addEventListener('submit', async(e) => {
-  e.preventDefault();
-
-  // Get the form values
-  const addressToValue = addressTo.value;
-  const ETHAmountValue = ETHAmount.value;
-  // Calculate amount to transfer in wei
-  const weiAmountValue = ethers.utils.parseEther(ETHAmountValue) //parseInt(ETHAmountValue) * 10**18
-
-  // Form the transaction request for sending ETH
-  const transactionRequest = {
-    to: addressToValue.toString(),
-    value: weiAmountValue.toString()
-  }
-
-  // Send the transaction and log the receipt
-  const receipt = await signer.sendTransaction(transactionRequest);
-  console.log(receipt);
-
-})
-
 // Initialise the page objects to interact with
 const ethereumButton = document.querySelector('.enableEthereumButton');
-const showAccount = document.querySelector('.showAccount');
-const showChainId = document.querySelector('.showChainId');
 
 // Initialise the active account and chain id
 let activeAccount;
@@ -48,7 +20,6 @@ let activeChainId;
 // Update the account and chain id when user clicks on button
 ethereumButton.addEventListener('click', () => {
   getAccount();
-  getChainId();
 });
 
 // Get the account in the window object
@@ -60,15 +31,7 @@ async function getAccount() {
   } else if (accounts[0] !== activeAccount) {
     activeAccount = accounts[0];
   }
-  showAccount.innerHTML = activeAccount;
-}
-
-// Get the connected network chainId
-async function getChainId() {
-    activeChainId = await ethereum.request({ method: 'eth_chainId' });
-    showChainId.innerHTML = activeChainId;
 }
 
 // Update the selected account and chain id on change
 ethereum.on('accountsChanged', getAccount);
-ethereum.on('chainChanged', getChainId);
