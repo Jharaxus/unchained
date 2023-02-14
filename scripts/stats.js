7eac7c53b6522317f72c80b15f159fc00a5c367e
Jaraxus <Superseth794@gmail.com>
2023-02-15 00:11:15 +0100

    Improve colors used in alpha-strategy page

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,13 +1,20 @@
 const distributionChart = document.getElementById('Distribution-Chart');
 
 const distributionData = {
-  labels: ['Alpha', 'Beta', 'Gamma', 'Delta'],
+  labels: ['Alpha', 'Beta', 'Gamma'],
   datasets: [{
       label: 'funds owned',
-      data: [12, 19, 3, 5],
-      borderWidth: 1
+      data: [12, 19, 7],
+      borderWidth: 1,
+      borderColor: '#0000000',
+      color: 'rgb(31, 33, 73)',   // --main-bg-color
+      backgroundColor: [
+        'rgb(41, 103, 48)', // --bg-light-1-color
+        'rgb(70, 55, 117)',  // --bg-light-2-color
+        'rgb(56, 97, 130)'  // --bg-light-3-color
+      ]
     }]
-  }
+}
 
 new Chart(distributionChart, {
   type: 'pie',
