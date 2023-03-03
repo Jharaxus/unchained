fd884ec016a24f7f7cd921acc9b65cff2a7dc432
Lucas Majerczyk <lucas@MacBook-Pro-de-Lucas.local>
2023-03-03 11:05:53 +0100

    shooting stars finised, just need you guys to review them

--- scripts/shooting_stars.js
+++ scripts/shooting_stars.js
@@ -14,52 +14,115 @@ function animateRayOfLighttt(delimiterElement, rayOfLightClass) {
 setInterval(() => {
   const delimiterElement = document.querySelector('.shootingStar3');
   animateRayOfLighttt(delimiterElement, 'ray-of-light3');
-}, 3000);
+}, 8000);
 
 setInterval(() => {
   const delimiterElement = document.querySelector('.shootingStar5');
   animateRayOfLighttt(delimiterElement, 'ray-of-light5');
-}, 3000);
+}, 8000);
 
 setInterval(() => {
   const delimiterElement = document.querySelector('.shootingStar7');
   animateRayOfLighttt(delimiterElement, 'ray-of-light7');
-}, 3000);
+}, 8000);
 
 setInterval(() => {
   const delimiterElement = document.querySelector('.shootingStar9');
   animateRayOfLighttt(delimiterElement, 'ray-of-light9');
-}, 3000);
+}, 8000);
 
 setInterval(() => {
   const delimiterElement = document.querySelector('.shootingStar11');
   animateRayOfLighttt(delimiterElement, 'ray-of-light11');
-}, 3000);
+}, 8000);
 
 setInterval(() => {
   const delimiterElement = document.querySelector('.shootingStar13');
   animateRayOfLighttt(delimiterElement, 'ray-of-light13');
-}, 3000);
-
-setInterval(() => {
-  const delimiterElement = document.querySelector('.shootingStar15');
-  animateRayOfLighttt(delimiterElement, 'ray-of-light15');
-}, 3000);
+}, 8000);
 
 setInterval(() => {
   const delimiterElement = document.querySelector('.shootingStar17');
   animateRayOfLighttt(delimiterElement, 'ray-of-light17');
-}, 3000);
+}, 8000);
 
 setInterval(() => {
   const delimiterElement = document.querySelector('.shootingStar19');
   animateRayOfLighttt(delimiterElement, 'ray-of-light19');
-}, 3000);
+}, 8000);
+
+setInterval(() => {
+  const delimiterElement = document.querySelector('.shootingStar21');
+  animateRayOfLighttt(delimiterElement, 'ray-of-light21');
+}, 8000);
+
+setInterval(() => {
+  const delimiterElement = document.querySelector('.shootingStar23');
+  animateRayOfLighttt(delimiterElement, 'ray-of-light23');
+}, 8000);
+
+
 
 // Set an interval to animate the ray of light every 2 seconds with a delay of 1 second for the second delimiter element
 setInterval(() => {
   const delimiterElement = document.querySelector('.shootingStar4');
   setTimeout(() => {
     animateRayOfLighttt(delimiterElement, 'ray-of-light4');
-  }, 1000);
-}, 3000);
+  }, 4000);
+}, 8000);
+
+setInterval(() => {
+  const delimiterElement = document.querySelector('.shootingStar6');
+  setTimeout(() => {
+    animateRayOfLighttt(delimiterElement, 'ray-of-light6');
+  }, 4000);
+}, 8000);
+
+setInterval(() => {
+  const delimiterElement = document.querySelector('.shootingStar8');
+  setTimeout(() => {
+    animateRayOfLighttt(delimiterElement, 'ray-of-light8');
+  }, 4000);
+}, 8000);
+
+setInterval(() => {
+  const delimiterElement = document.querySelector('.shootingStar10');
+  setTimeout(() => {
+    animateRayOfLighttt(delimiterElement, 'ray-of-light10');
+  }, 4000);
+}, 8000);
+
+setInterval(() => {
+  const delimiterElement = document.querySelector('.shootingStar12');
+  setTimeout(() => {
+    animateRayOfLighttt(delimiterElement, 'ray-of-light12');
+  }, 4000);
+}, 8000);
+
+setInterval(() => {
+  const delimiterElement = document.querySelector('.shootingStar14');
+  setTimeout(() => {
+    animateRayOfLighttt(delimiterElement, 'ray-of-light14');
+  }, 4000);
+}, 8000);
+
+setInterval(() => {
+  const delimiterElement = document.querySelector('.shootingStar16');
+  setTimeout(() => {
+    animateRayOfLighttt(delimiterElement, 'ray-of-light16');
+  }, 4000);
+}, 8000);
+
+setInterval(() => {
+  const delimiterElement = document.querySelector('.shootingStar18');
+  setTimeout(() => {
+    animateRayOfLighttt(delimiterElement, 'ray-of-light18');
+  }, 4000);
+}, 8000);
+
+setInterval(() => {
+  const delimiterElement = document.querySelector('.shootingStar20');
+  setTimeout(() => {
+    animateRayOfLighttt(delimiterElement, 'ray-of-light20');
+  }, 4000);
+}, 8000);
