d5be21a2c6554f5ed1ef8e83622a547addf9b309
Jharaxus <Superseth794@gmail.com>
2023-03-12 20:57:27 +0100

    Fix merge

--- front-end/scripts/strategy-alpha/call.js
+++ front-end/scripts/strategy-alpha/call.js
@@ -6,6 +6,7 @@ const dotRightDeposit2 = document.querySelector('.dot.right-deposit2');
 const lineDeposit1 = document.querySelector('.line-deposit1');
 const lineDeposit2 = document.querySelector('.line-deposit2');
 
+<<<<<<< HEAD
 const approvalWordDeposit = document.querySelector('.approval-word-deposit');
 const delegateWordDeposit = document.querySelector('.delegate-word-deposit');
 const completedWordDeposit = document.querySelector('.completed-word-deposit');
@@ -16,8 +17,10 @@ const web3 = new Web3(Web3.givenProvider);
 let tokenAddress = "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9";
 let walletAddress = "0x75741c5D7AD1BA3881001F0A4C53CaA0016694Fa";
 
+=======
+>>>>>>> 27dec97 (Fix errors)
 // The minimum ABI to get ERC20 Token balance
-let minABI = [
+let erc20ABI = [
   // balanceOf
   {
     "constant":true,
@@ -35,125 +38,124 @@ let minABI = [
     "type":"function"
   }
 ];
-//let contractAddress = 0x47c05bcca7d57c87083eb4e586007530ee4539e9;
 
-let contract = new web3.eth.Contract(minABI,tokenAddress);
+// ABI for our alpha vault contract
+let AsvABI = [
+  {
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_delegator",
+        "type": "address"
+      }
+    ],
+    "name": "assetsDelegated",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_delegator",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "delegate",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "name",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "totalAssetsDelegated",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_receiver",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "withdraw",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
 
-async function getBalance() {
-  balance = await contract.methods.balanceOf(walletAddress).call(); // changer balanceOf par la methode dont j'ai besoin 
+let aaveAddress = "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9";
+let vaultAddress = "0x47c05bcca7d57c87083eb4e586007530ee4539e9";
+
+const web3 = new Web3(Web3.givenProvider);
+
+let aaveContract = new web3.eth.Contract(erc20ABI, aaveAddress);
+
+activeAccount = null;
+
+async function getAaveBalance() {
+  balance = await aaveContract.methods.balanceOf(activeAccount).call(); // changer balanceOf par la methode dont j'ai besoin 
   const balanceConverted = balance / 10**16;
   return balanceConverted;
 }
 
-/*CurrentDeposit = document.getElementById("wallet-withdraw-strategy-balance");
-
-window.addEventListener('addressToCatch', function(event) {
-  getBalance().then(function (result) {
-    CurrentDeposit.textContent = result;
-    const withdrawBalanceWorthSend = new CustomEvent('UpdateDeposit', { detail: result});
-    window.dispatchEvent(withdrawBalanceWorthSend);
-  });
-});*/
-
 walletBalance = document.getElementById("wallet-deposit-token-balance");
 
 window.addEventListener('addressToCatch', function(event) {
-    getBalance().then(function (result) {
+  activeAccount = event.detail;
+  console.log(activeAccount);
+  getAaveBalance().then(function (result) {
     walletBalance.textContent = result;
-    const walletBalanceWorthSend = new CustomEvent('UpdateDeposit', { detail: result});
+    const walletBalanceWorthSend = new CustomEvent('UpdateDeposit', {detail: result});
     window.dispatchEvent(walletBalanceWorthSend);
   });
 });
 
-let AsvABI =
-  [
-    {
-      "inputs": [],
-      "stateMutability": "nonpayable",
-      "type": "constructor"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "_delegator",
-          "type": "address"
-        }
-      ],
-      "name": "assetsDelegated",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "_delegator",
-          "type": "address"
-        },
-        {
-          "internalType": "uint256",
-          "name": "_amount",
-          "type": "uint256"
-        }
-      ],
-      "name": "delegate",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "name",
-      "outputs": [
-        {
-          "internalType": "string",
-          "name": "",
-          "type": "string"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "totalAssetsDelegated",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "_receiver",
-          "type": "address"
-        },
-        {
-          "internalType": "uint256",
-          "name": "_amount",
-          "type": "uint256"
-        }
-      ],
-      "name": "withdraw",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    }
-  ];
 /*
 let contractAlphaStrategyVault = new web3.eth.Contract(AsvABI, "lucas", tokenAddress, );
 
@@ -175,4 +177,8 @@ window.addEventListener('DepositButtonActivated', () =>{
   approvalWordDeposit.style.color = 'snow';
   delegateWordDeposit.style.color = 'snow';
   completedWordDeposit.style.color = 'snow';
+  lineDeposit1.style.backgroundColor = 'background: linear-gradient(to right, rgba(114, 224, 86, 0.402), rgb(2, 9, 26))'
+  lineDeposit2.style.backgroundColor = 'background: linear-gradient(to right, rgba(114, 224, 86, 0.402), rgb(2, 9, 26))'
+
+
 });*/
--- web3-backend/script.sh
+++ web3-backend/script.sh
@@ -1,7 +1,9 @@
 export BINANCE=0xBE0eB53F46cd790Cd13851d5EFf43D12404d33E8
 export ALICE=0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266
 export BOB=0x70997970c51812dc3a010c7d01b50e0d17dc79c8
+
 export LUCAS=0x75741c5D7AD1BA3881001F0A4C53CaA0016694Fa
+export HUGO=0xB6959FC71F64df777de2E8AB58F99c2505798Ce4
 
 export ALPHA_VAULT=0x47c05bcca7d57c87083eb4e586007530ee4539e9
 export AAVE=0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9
@@ -17,10 +19,18 @@ echo "Balance done"
 cast rpc anvil_impersonateAccount $BINANCE
 cast send $AAVE --from $BINANCE "transfer(address,uint256)(bool)" $ALICE $BINANCE_BALANCE_A
 cast send $AAVE --from $BINANCE "transfer(address,uint256)(bool)" $BOB $BINANCE_BALANCE_B
-cast send $AAVE --from $BINANCE "transfer(address,uint256)(bool)" $LUCAS $BINANCE_BALANCE_A
 
-echo "Transfer done"
+cast send $AAVE --from $BINANCE "transfer(address,uint256)(bool)" $LUCAS $BINANCE_BALANCE_A
+cast send $AAVE --from $BINANCE "transfer(address,uint256)(bool)" $HUGO $BINANCE_BALANCE_A
+
+echo "transfer Aave done"
+
+cast send $LUCAS --from $BINANCE --value 100ether
+cast send $HUGO  --from $BINANCE --value 100ether
+
+echo "Transfer ether done"
 
 cast call $AAVE "balanceOf(address)(uint256)" $ALICE
 cast call $AAVE "balanceOf(address)(uint256)" $BOB
 cast call $AAVE "balanceOf(address)(uint256)" $LUCAS
+cast call $AAVE "balanceOf(address)(uint256)" $HUGO
